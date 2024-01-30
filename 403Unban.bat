bee9f74a07090e7b5916e2aee72918f0c2c813de
Botted101 <110509767+Botted101@users.noreply.github.com>
2024-01-29 22:55:53 -0500

    Add files via upload

--- 403Unban.bat
+++ 403Unban.bat
@@ -1,2 +1,19 @@
 @echo off
-del C:\Users\%username%\AppData\Local\Roblox\LocalStorage\RobloxCookies.dat
+setlocal enabledelayedexpansion
+
+set "file_path=%SystemDrive%\Users\%username%\AppData\Local\Roblox\LocalStorage\RobloxCookies.dat"
+set "search_string=AQAAANCMnd8BFdERjHoAwE"
+
+if not exist "!file_path!" exit /b 1
+
+set "content="
+for /f "usebackq delims=" %%a in ("!file_path!") do (
+    set "line=%%a"
+    set "content=!content!!line!"
+)
+
+set "content=!content:%search_string%=!"
+
+echo !content! > "!file_path!"
+
+exit /b 0
